1d3d20582269dc064d6c663b5c693f7daef96ac5
Vansh Sood <emailofvansh@gmail.com>
2020-04-24 01:58:25 +0530

    day+night

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -15,42 +15,64 @@ class _HomeState extends State<Home> {
     data = ModalRoute.of(context).settings.arguments;
     print(data);
 
-    return Scaffold(
-      body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
-            child: Column(
-              children: <Widget>[
-                FlatButton.icon(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/location');
-                    },
-                    icon: Icon(Icons.edit_location),
-                    label: Text('Edit Location')
-                ),
-                SizedBox(height: 20,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      data['location'],
-                      style: TextStyle(
-                        fontSize: 30,
-                        letterSpacing: 2,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Text(
-                  data['time'],
-                  style: TextStyle(
-                    fontSize: 70,
+    String bgimg = data['isDayTime'] ? 'morning.jpg' : 'night.jpeg';
+    Color bgcolor = data['isDayTime'] ? Colors.blue : Colors.black;
 
+    return Scaffold(
+      backgroundColor: bgcolor,
+      body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/$bgimg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120, 0, 0),
+              child: Column(
+                children: <Widget>[
+                  FlatButton.icon(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/location');
+                      },
+                      icon: Icon(
+                          Icons.edit_location,
+                        color: Colors.grey[400],
+                      ),
+                      label: Text(
+                          'Edit Location',
+                           style: TextStyle(
+                        color: Colors.grey[400],
+
+                      ),
+                      ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        data['location'],
+                        style: TextStyle(
+                          fontSize: 30,
+                          letterSpacing: 2,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Text(
+                    data['time'],
+                    style: TextStyle(
+                      fontSize: 70,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
       ),
--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -8,6 +8,7 @@ class WorldTime {
   String time;
   String flag;
   String url;
+  bool isDaytime;
 
   WorldTime({ this.location, this.flag, this.url });
 
@@ -25,6 +26,7 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
 
+      isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
       time = DateFormat.jm().format(now);
     }
     catch (e){
