45b56ea52ed11f7919a36f484883a489284ad1c2
Vansh Sood <emailofvansh@gmail.com>
2020-04-24 08:40:08 +0530

    Final build

--- lib/pages/location.dart
+++ lib/pages/location.dart
@@ -12,12 +12,18 @@ class _ChooseLocationState extends State<ChooseLocation> {
   List<WorldTime> locations =[
     WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
     WorldTime(url: 'America/Los_Angeles', location: 'Athens', flag: 'usa.png'),
-    WorldTime(url: 'Asia/Kolkata', location: 'New Delhi', flag: 'india.png'),
+//    WorldTime(url: 'Asia/Kolkata', location: 'New Delhi', flag: 'india.png'),
     WorldTime(url: 'Europe/Berlin', location: 'Berlin', flag: 'germany.png'),
-    WorldTime(url: 'Europe/Shanghai', location: 'Shanghai', flag: 'china.png'),
+    WorldTime(url: 'Asia/Shanghai', location: 'Shanghai', flag: 'china.png'),
     WorldTime(url: 'Europe/Madrid', location: 'Madrid', flag: 'spain.png'),
     WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
     WorldTime(url: 'Australia/Sydney', location: 'Sydney', flag: 'australia.png'),
+    WorldTime(url: 'America/Argentina/Buenos_Aires', location: 'Buenos Aires', flag: 'argentina.png'),
+    WorldTime(url: 'America/Mexico_City', location: 'Mexico City', flag: 'mexico.png'),
+    WorldTime(url: 'Asia/Bangkok', location: 'Bangkok', flag: 'thailand.png'),
+    WorldTime(url: 'Asia/Dubai', location: 'Dubai', flag: 'uae.png'),
+//    WorldTime(url: 'Australia/Sydney', location: 'Sydney', flag: 'australia.png'),
+//    WorldTime(url: 'Australia/Sydney', location: 'Sydney', flag: 'australia.png'),
   ];
 
   void updateTime(index) async {
